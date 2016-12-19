fcb072fd6dae60d901dda1ea4517d385c2cb5ca0
你的名字 <email>
2016-12-19 14:00:12 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,17 +17,23 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
+
+
 
   def update
     @group = Group.find(params[:id])
-
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update success"
+    else
+      render :edit
+    end
   end
 
     def destroy
